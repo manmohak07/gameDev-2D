75ca5037557b31b022c8e9a3a6fe6c802d495b19
Mohak Agrawal <112922671+manmohak07@users.noreply.github.com>
2024-03-18 19:11:10 +0530

    Add files via upload
    Camera movement script

---
+++ Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float cameraSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
+    }
+}
